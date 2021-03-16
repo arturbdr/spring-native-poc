eee59278b2258ae64b640f195e914e642a986ba1
Artur Drummond <arturbdr@gmail.com>
2021-03-16 20:05:45 +0100

    initial commit

---
+++ src/test/java/com/poc/springnative/SpringnativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.springnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringnativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
