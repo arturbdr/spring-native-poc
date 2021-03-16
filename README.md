93ff9f8f7f31bec6c2c4638127b335d82b51b398
Artur Drummond <arturbdr@gmail.com>
2021-03-16 20:15:33 +0100

    feat:
    added readme and a simple controller with hello world

---
+++ README.md
@@ -0,0 +1,11 @@
+### Simple Spring Native poc
+
+To start in command line:
+```shell
+  mvn spring-aot:generate spring-boot:run
+```
+
+This will start the application in port 8080.
+
+Check if the hello world is working by consuming:
+`curl http://localhost:8080/hello`
---
+++ src/main/java/com/poc/springnative/gateway/controller/HelloSpringNativeController.java
@@ -0,0 +1,15 @@
+package com.poc.springnative.gateway.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSpringNativeController {
+
+
+    @GetMapping("hello")
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello world");
+    }
+}
